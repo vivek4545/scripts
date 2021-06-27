e4071753924f04fa7e3b3da389b7ce9e9294b691
VK-2 <vk-2@localhost.localdomain>
2021-06-27 11:40:16 -0400

    Updated jenkins install script

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
  sudo yum install java -y
+ cd ~
  wget https://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.68/bin/apache-tomcat-8.5.68.tar.gz
- sudo mkdir apps
- sudo chmod 777 apps/
+ sudo mkdir /apps
+ sudo chmod 777 /apps/
+ cd /apps
  mkdir tomcat
  tar -xvf ~/apache-tomcat-8.5.68.tar.gz
  cp -r apache-tomcat-8.5.68/* tomcat/
