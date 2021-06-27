0199211f7cf651b1a63f1d4582b672f38ce5292e
technicalguftgu <f16.vivek@gmail.com>
2021-06-27 20:43:32 +0530

    This script will install jenkins on server

---
+++ install_jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ sudo yum install java -y
+ wget https://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.68/bin/apache-tomcat-8.5.68.tar.gz
+ sudo mkdir apps
+ sudo chmod 777 apps/
+ mkdir tomcat
+ tar -xvf ~/apache-tomcat-8.5.68.tar.gz
+ cp -r apache-tomcat-8.5.68/* tomcat/
+ rm -rf apache-tomcat-8.5.68/
+ cd tomcat/webapps/
+ wget https://get.jenkins.io/war-stable/2.289.1/jenkins.war
+ cd ../bin
+ ./catalina.sh start
+ sudo service firewalld stop
